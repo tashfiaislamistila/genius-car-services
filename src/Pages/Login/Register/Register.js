b759aec756d8c33869c95adadfca819335dceb09
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-13 03:20:23 -0700

    navigate proceed checkout,sign out and some modification commit

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './Register.css'
+import './Register.css';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
     const navigate = useNavigate();
     const navigateLogin = () => {
         navigate('/login');
     }
+    if (user) {
+        navigate('/home');
+    }
+
     const handlerRegister = event => {
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
+
+        createUserWithEmailAndPassword(email, password);
     }
     return (
         <div className='register-form'>
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
     return (
         <div>
             <h2>Welcome to detail: {serviceId}</h2>
+            <div className='text-center'>
+                <Link to='/checkout'>
+                    <button className='btn btn-primary'>Proceed Checkout</button>
+                </Link>
+            </div>
         </div>
     );
 };
