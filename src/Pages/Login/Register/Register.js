b160ae59cd84bae38aaffec16eae2d40dc41d919
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-13 16:29:42 -0700

    error message handle and google sign in set commit

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import './Register.css';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
 const Register = () => {
     const [
         createUserWithEmailAndPassword,
@@ -39,6 +40,7 @@ const Register = () => {
             </form>
             <p>Already have an account ?<Link to='/login' className='text-danger pe-auto text-decoration-none'
                 onClick={navigateLogin}>Please Login</Link></p>
+            <SocialLogin></SocialLogin>
         </div >
     );
 };
--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import google from '../../../images/social/google.png'
+import facebook from '../../../images/social/facebook.png'
+import github from '../../../images/social/Github.png'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useNavigate, useNavigationType } from 'react-router-dom';
+
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    let errorElement;
+    if (error) {
+        errorElement = <div>
+            <p className='text-danger'>Error: {error.message}</p>
+        </div>
+    }
+    if (user) {
+        navigate('/home');
+    }
     return (
         <div>
             <div className='d-flex align-items-center'>
@@ -8,10 +25,21 @@ const SocialLogin = () => {
                 <p className='mt-2 px-2'>or</p>
                 <div style={{ height: '1px' }} className='bg-primary w-50'></div>
             </div>
+            {errorElement}
             <div>
-                <button className='btn btn-info w-50 d-block mx-auto'>
+                <button
+                    onClick={() => signInWithGoogle()}
+                    className='btn btn-info w-50 d-block mx-auto my-2'>
                     <img style={{ width: '30px' }} src={google} alt="" />
                     <span className='px-2'> Google Sign In</span>
+                </button>
+                <button className='btn btn-info w-50 d-block mx-auto my-2'>
+                    <img style={{ width: '30px' }} src={facebook} alt="" />
+                    <span className='px-2'> Facebook Sign In</span>
+                </button>
+                <button className='btn btn-info w-50 d-block mx-auto'>
+                    <img style={{ width: '30px' }} src={github} alt="" />
+                    <span className='px-2'> Github Sign In</span>
                 </button>
             </div>
         </div >
