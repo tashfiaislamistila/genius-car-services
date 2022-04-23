1562abd3c31006903dd23205d7fda22ddd98191a
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-23 15:03:01 -0700

    conneted with db and some other modifications

--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -15,7 +15,8 @@ const RequireAuth = ({ children }) => {
     if (!user) {
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
-    if (!user.emailVerified) {
+
+    if (user.providerData[0]?.providerId === 'password' && !user.emailVerified) {
         return <div>
             <h3 className='text-danger'>Your email is not verified!!</h3>
             <h5 className='text-success'>Please verify your your email address </h5>
